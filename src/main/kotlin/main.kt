1f5982d6dc61be5ed8bdd6cbdc32166d2d5a48d9
Maksim Shcherbakov <i@max87.ru>
2024-02-25 07:56:45 +0300

    Fix syntax added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,8 +4,8 @@ fun main() {
     val people = "людям"
     println("Введите колличество лайков: ")
     val likes = readln().toInt()
-    val lastNuberOfLikes = likes % 10
+    val lastNumberOfLikes = likes % 10
     val lastTwoNumberOfLikes = likes % 100
-    val manOrPeople = if (lastNuberOfLikes == 1 && lastTwoNumberOfLikes != 11) man else people
+    val manOrPeople = if (lastNumberOfLikes == 1 && lastTwoNumberOfLikes != 11) man else people
     println("Понравилось: $likes $manOrPeople")
 }
